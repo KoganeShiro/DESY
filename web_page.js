c1efad4014c345612f689b397116b7ce3773ed35
K Shiro <126095786+KoganeShiro@users.noreply.github.com>
2024-03-18 18:52:47 +0100

    open the camera and the mic

--- web_page.js
+++ web_page.js
@@ -1,1 +1,21 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+
+window.onload = function() {
+    var constraints = { audio: true, video: true };
+
+    // Call getUserMedia
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(mediaStream) {
+            var video = document.querySelector('video');
+            video.srcObject = mediaStream;
+            video.play();
+        })
+        .catch(function(err) {
+            if (err.name === 'NotAllowedError' || err.name === 'PermissionDeniedError') {
+                console.log("User denied access to camera and microphone");
+            } else {
+                console.log("An error occurred: " + err.message);
+            }
+        });
+}
 
