a328a0665b1b64d76cbf592769cf1da4afb0b7e3
K Shiro <126095786+KoganeShiro@users.noreply.github.com>
2024-03-19 15:41:35 +0100

    banner

--- web_page.js
+++ web_page.js
@@ -19,3 +19,49 @@ window.onload = function() {
         });
 }
 
+
+/*
+window.onload = function() {
+    var constraints = { audio: true, video: true };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(mediaStream) {
+            var video = document.createElement('video');
+            video.srcObject = mediaStream;
+            video.play();
+
+            var canvas = document.createElement('canvas');
+            var ctx = canvas.getContext('2d');
+
+            video.addEventListener('loadedmetadata', function() {
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+
+                document.body.appendChild(canvas);
+
+                function processFrame() {
+                    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+                    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                    var data = imageData.data;
+                    for (var i = 0; i < data.length; i += 4) {
+                        data[i] = 255 - data[i]; // Red
+                        data[i + 1] = 255 - data[i + 1]; // Green
+                        data[i + 2] = 255 - data[i + 2]; // Blue
+                        // data[i + 3] is the alpha channel; we leave it unchanged
+                    }
+                    ctx.putImageData(imageData, 0, 0);
+                    requestAnimationFrame(processFrame);
+                }
+
+                processFrame();
+            });
+        })
+        .catch(function(err) {
+            if (err.name === 'NotAllowedError' || err.name === 'PermissionDeniedError') {
+                console.log("User denied access to camera and microphone");
+            } else {
+                console.log("An error occurred: " + err.message);
+            }
+        });
+}
+*/
