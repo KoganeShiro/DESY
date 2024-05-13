796ceadb58275a3522fdba10867f6714e5e23555
Louwick <loick.bainachi@gmail.com>
2024-05-14 01:50:52 +0200

    page about + finitions boutons css + ajustement Nav

--- DESY/src/app/pages/about/about.component.ts
+++ DESY/src/app/pages/about/about.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { GradientBackgroundComponent } from "../../components/gradient-background/gradient-background.component";
 import { BlockAboutComponent } from "../../components/block-about/block-about.component";
 
 @Component({
@@ -7,7 +6,7 @@ import { BlockAboutComponent } from "../../components/block-about/block-about.co
     standalone: true,
     templateUrl: './about.component.html',
     styleUrl: './about.component.css',
-    imports: [GradientBackgroundComponent, BlockAboutComponent]
+    imports: [ BlockAboutComponent]
 })
 export class AboutComponent {
 
