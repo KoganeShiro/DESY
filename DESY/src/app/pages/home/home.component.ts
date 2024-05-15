9800966e32650803d40515a14926a698936e008f
Louwick <loick.bainachi@gmail.com>
2024-05-15 20:51:10 +0200

    Premier merging avec Camera

--- DESY/src/app/pages/home/home.component.ts
+++ DESY/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NameComponent } from '../../components/name/name.component';
 import { GalleryButtonComponent } from '../../components/gallery-button/gallery-button.component';
+import { CameraComponent } from '../../components/camera/camera.component';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { GalleryButtonComponent } from '../../components/gallery-button/gallery-
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [NameComponent, GalleryButtonComponent]
+    imports: [NameComponent, GalleryButtonComponent, CameraComponent]
 })
 export class HomeComponent {
     isVideo : boolean;
